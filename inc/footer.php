3791e28ddf24a63f834390aaee2f37b7ce993878
Kraynitexe <apinga.sampilahy@gmail.com>
2025-07-14 13:11:09 +0300

    Ajout deconnection et amelioration UI

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,4 @@
-<footer></footer>
+<footer>
+    <p>&copy;BORROW 2025 - ETU004214 - ETU004036</p>
+    <a href="https://github.com/Kraynitexe/ExamenS">Lien Github</a>
+</footer>
--- inc/navbar.php
+++ inc/navbar.php
@@ -3,7 +3,7 @@ require_once "header.php";
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../pages/index.php">Accueil</a>
+    <a class="navbar-brand" href="../pages/index.php">BORROW</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,7 +13,7 @@ require_once "header.php";
           <a class="nav-link" href="../pages/index.php">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="deconnexion.php">Deconnexion</a>
+          <a class="nav-link" href="../inc/deconnexion.php">Deconnexion</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="liste.php">Liste des objets</a>
